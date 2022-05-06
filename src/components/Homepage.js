f34815c37abfb25064524d642f279b570ac64b86
Michael Klein <Michaelklein1997@gmail.com>
2022-05-06 15:43:51 -0500

    initial commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,31 @@
+import { Fragment, useEffect, useState } from "react";
+
+const Homepage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    fetch("https://recipe-api-mk.herokuapp.com/recipes")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setRecipes(data);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h1>{recipe.title}</h1>
+            <p>Servings: {recipe.servings}</p>
+            <p>Prep time: {recipe.prepTime}</p>
+            <p>Cook time: {recipe.cookTime}</p>
+            <p>Category: {recipe.category}</p>
+          </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Homepage;
